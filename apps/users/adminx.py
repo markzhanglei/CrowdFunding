e702472fd459bd581402b17d0371c6f6af2ce749
1158098117@qq.com <1158098117@qq.com>
2018-05-15 20:07:59 +0800

    Initial commit

---
+++ apps/users/adminx.py
@@ -0,0 +1,24 @@
+import xadmin
+from xadmin import views
+
+from users.models import UserProfile, UserAddress
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "烽火众筹平台"
+    site_footer = "烽火众筹平台"
+    menu_style = 'accordion'
+
+
+xadmin.site.unregister(UserProfile)
+xadmin.site.register(UserProfile)
+xadmin.site.register(UserAddress)
+
+
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
